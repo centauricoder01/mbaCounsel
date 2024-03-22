9f116d002bd6248fe62d8b4b5eef38c5cb76b5fc
Rajendra <itsrajendrapatel@gmail.com>
2024-03-22 15:16:58 +0530

    Signup page design done and also taking the value

--- src/Components/Authtication/Signup/Signup.jsx
+++ src/Components/Authtication/Signup/Signup.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 import { FcGoogle } from "react-icons/fc";
-import { BsFacebook } from "react-icons/bs";
-import { ImLinkedin } from "react-icons/im";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [signupUser, setSignupUser] = useState({
+    name: "",
+    email: "",
+    phoneNo: "",
+    currentEducation: "",
+    CourseLooking: "",
+    password: "",
+    comfirmpassword: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    // Update the state immediately
+    setSignupUser((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // checking the values
+    if (signupUser.password !== signupUser.comfirmpassword) {
+      alert("Password are not matching, please try again...");
+    } else {
+      console.log(signupUser);
+    }
+  };
   return (
     <div className="main-signup-design">
       <div className="left-side-div">
@@ -20,19 +46,69 @@ const Signup = () => {
         <p className="create-account-p">Create a Acccount</p>
         <div className="signup-icons-div">
           <FcGoogle size={30} cursor={"pointer"} />
-          <ImLinkedin size={30} color="#0063c8" cursor={"pointer"} />
-          <BsFacebook size={30} color="#0866ff" cursor={"pointer"} />
         </div>
         <div className="or-tag-div">
           <hr />
           <p>OR</p>
           <hr />
         </div>
-        <form className="signup-form-div">
-          <input type="text" placeholder="Full name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Current Password" />
+        <form className="signup-form-div" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Full name*"
+            name="name"
+            value={signupUser.name}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="number"
+            placeholder="Phone No*"
+            name="phoneNo"
+            value={signupUser.phoneNo}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Currently Studying*"
+            name="currentEducation"
+            value={signupUser.currentEducation}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Course Looking*"
+            name="CourseLooking"
+            value={signupUser.CourseLooking}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email*"
+            name="email"
+            value={signupUser.email}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password*"
+            name="password"
+            value={signupUser.password}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="password"
+            placeholder="confirm Password*"
+            name="comfirmpassword"
+            value={signupUser.comfirmpassword}
+            onChange={handleInputChange}
+            required
+          />
           <button type="submit">Create Account</button>
         </form>
         <p className="already-have-account">
