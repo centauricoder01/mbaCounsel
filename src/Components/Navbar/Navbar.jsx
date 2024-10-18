3ebc9d6296cc156ce0e9e00201b417b4e1bd3c09
Rajendra <itsrajendrapatel@gmail.com>
2024-10-18 12:25:28 +0530

    Pushed navbar changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -148,19 +148,11 @@ const MainNavbar = () => {
             </DropDownMenu>
             {/* RESOURCES */}
             <DropDownMenu title="Resources" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Web Development
+              <NavDropdown.Item href="/latestnews">
+                Latest News
               </NavDropdown.Item>
-              <DropDownSubMenu href="#action/3.7" title="Deisgn">
-                <NavDropdown.Item href="#action/8.1">Html</NavDropdown.Item>
-                <NavDropdown.Item href="#action/8.2">CSS</NavDropdown.Item>
-                {/* <DropDownSubMenu href="#action/3.7" title="Development">
-                  <NavDropdown.Item href="#action/9.1">
-                    Javascript
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/9.2">Jquery</NavDropdown.Item>
-                </DropDownSubMenu> */}
-              </DropDownSubMenu>
+              <NavDropdown.Item href="/articles">Article</NavDropdown.Item>
+              <NavDropdown.Item href="/blogs">Blog</NavDropdown.Item>
             </DropDownMenu>
             <div>
               <input
